73ab72e8d1821d45481ce046089137667fca47af
Karan <soha0021@algonquinlive.com>
2023-12-05 23:43:05 -0500

    Fixed the username issue where the username was going out of the viewport

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -23,20 +23,21 @@ export default function Profile({ userId }) {
   return (
     <>
       <div className="container mx-auto">
-        <div className="heading grid md:grid-cols-2 sm:grid-cols-1 container text-left my-10">
+        <div className="heading container text-left my-10">
           <img
             src={
               posts.length > 0 ? posts[0]?.creator.image : session?.user.image
             }
-            className="rounded-full ml-3"
+            className="rounded-full m-3"
             alt=""
           />
           <h1
-            className={`font-bold text-6xl ml-5 bg-clip-text text-transparent bg-gradient-to-r from-orange-400 via-orange-500 to-orange-300 mt-3`}
+            className={`font-bold text-6xl ml-5 bg-clip-text text-transparent bg-gradient-to-r from-orange-400 via-orange-500 to-orange-300 py-3`}
           >
             {posts.length > 0
-              ? posts[0]?.creator.username.toLowerCase().replace(" ", "")
-              : session?.user.name.toLowerCase().replace(" ", "")}
+              ? posts[0]?.creator.username.toLowerCase().split(" ")[0]
+              : session?.user.name.toLowerCase().split(" ")[0]}
+              
           </h1>
         </div>
         <div className="grid container mx-auto sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 text-left mt-4">
